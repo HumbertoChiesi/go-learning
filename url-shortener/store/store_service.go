a6dc1e7332096d955f75afc0baf8a852918bf62d
HumbertoChiesi <66853228+HumbertoChiesi@users.noreply.github.com>
2025-07-14 20:09:40 -0300

    Add files via upload

---
+++ url-shortener/store/store_service.go
@@ -0,0 +1,51 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+var (
+	storeService = &StorageService{}
+	ctx          = context.Background()
+)
+
+const CacheDuration = 6 * time.Hour
+
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping().Result() // removed ctx
+	if err != nil {
+		panic(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
+
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(shortUrl, originalUrl, CacheDuration).Err() // removed ctx
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+}
+
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(shortUrl).Result() // removed ctx
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
+	}
+	return result
+}
